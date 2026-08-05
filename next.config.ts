62b3aab533d62182e128b6fdcc4c3f6b9127f473
Bunyamin <bunyamin2002@mail.ru>
2026-08-05 22:31:58 +0400

    feat: add rewrite rule for menu routes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/:slug*",
+        destination: "https://admin.menovo.rest/:slug*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
